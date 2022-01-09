5213bd691a2850b5385c7d0d6362cf7ac9671b70
floKianB <kianbaban@yahoo.com>
2022-01-08 23:47:27 -0500

    basic timer and API set + wave

---
+++ script.js
@@ -0,0 +1,73 @@
+const API = 'http://api.quotable.io/random'
+const quoteDisplayElement = document.getElementById('quoteDisplay')
+const quoteInputElement = document.getElementById('quoteInput')
+const timerElement = document.getElementById('timer')
+
+quoteInputElement.addEventListener('input', () => {
+  const arrayQuote = quoteDisplayElement.querySelectorAll('span')
+  const arrayValue = quoteInputElement.value.split('')
+
+  let correct = true
+  arrayQuote.forEach((characterSpan, index) => {
+    const character = arrayValue[index]
+    if (character == null) {
+      characterSpan.classList.remove('correct')
+      characterSpan.classList.remove('incorrect')
+      correct = false
+    } else if (character === characterSpan.innerText) {
+      characterSpan.classList.add('correct')
+      characterSpan.classList.remove('incorrect')
+    } else {
+      characterSpan.classList.remove('correct')
+      characterSpan.classList.add('incorrect')
+      correct = false
+    }
+  })
+
+  if (correct) renderNewQuote()
+})
+
+function getRandomQuote() {
+  return fetch(API)
+    .then(response => response.json())
+    .then(data => data.content)
+}
+
+async function renderNewQuote() {
+  const quote = await getRandomQuote()
+  quoteDisplayElement.innerHTML = ''
+  quote.split('').forEach(character => {
+    const characterSpan = document.createElement('span')
+    characterSpan.innerText = character
+    quoteDisplayElement.appendChild(characterSpan)
+  })
+  quoteInputElement.value = null
+  startTimer()
+}
+
+let startTime
+function startTimer() {
+  timerElement.innerText = 0
+  startTime = new Date()
+  setInterval(() => {
+    timer.innerText = getTimerTime()
+    if (timer.innerText == 999){
+      refresh()
+    } 
+  }, 1000)
+}
+
+function getTimerTime() {
+
+  return Math.floor((new Date() - startTime) / 1000)
+}
+function refresh() {
+  window.location.reload();
+}
+
+window.onkeydown = function (event) {
+  if (event.which == 8 || event.which == 46) { 
+    event.preventDefault(); 
+  } };      
+
+renderNewQuote()
